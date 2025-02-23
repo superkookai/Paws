f8829fd5472f621d20690d8d523aa45336000615
Weerawut <weerawut@live.com>
2025-02-23 09:59:47 +0700

    Initial Commit

---
+++ Paws/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Paws
+//
+//  Created by Weerawut Chaiyasomboon on 23/2/2568 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Paws/PawsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawsApp.swift
+//  Paws
+//
+//  Created by Weerawut Chaiyasomboon on 23/2/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct PawsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
